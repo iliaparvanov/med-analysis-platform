cab57fca2b4c85dfa2cf89f9030ab5187a9ecd9e
iliaparvanov <ilia.nikolaev.parvanov@gmail.com>
2019-11-18 21:26:07 +0200

    Changed database to postgresql; I know the password and username for the db shouldn't be directly stored in the settings.py file, will fix later

--- med_analyser/med_analyser/settings.py
+++ med_analyser/med_analyser/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'med_analyser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'med_analyserdb',
+        'USER': 'med_analyseruser',
+        'PASSWORD': 'med_analyserpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
