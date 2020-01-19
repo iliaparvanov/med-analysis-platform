0de19bebd9503c931d12f903d151ca437a4fc36f
Ilia <ilia.nikolaev.parvanov@gmail.com>
2020-01-19 19:18:53 +0200

    Added models for Plan and Subscription

--- med_analyser/subscriptions/models.py
+++ med_analyser/subscriptions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Plan(models.Model):
+    
+    PLAN_TYPES = [
+        ('pro', 'Professional'),
+        ('free', 'Free')
+    ]
+    stripe_plan_id = models.CharField(max_length=50)
+    price = models.IntegerField(default=15)
+    plan_type = models.CharField(max_length=4, choices=PLAN_TYPES, default='free')
+
+class Subscription(models.Model):
+    stripe_subscription_id = models.CharField(max_length=50)
+    stripe_customer_id = models.CharField(max_length=50)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, null=True)
